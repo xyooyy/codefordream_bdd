e714d5d9843f7506bffb967bb3d285952f4f90b0
xyooyy <xyooyy@gmail.com>
2014-09-29 16:26:59 +0800

    删除无用文件夹

--- support/world.js
+++
@@ -1,7 +0,0 @@
-var HomePage = require('./google_land').Land;
-
-var CucumberJsExampleWorld = function() {
-  console.log("CucumberJsExampleWorld");
-  this.google = new GoogleLand();
-};
-exports.World = CucumberJsExampleWorld;
