96fde677d476869001b1e68e29ca84d8ccbd833d
weiketa <lyvoctor@gmail.com>
2014-11-09 21:29:46 +0800

    add one practice answer and test practice interface work

--- features/step_definitions/one_practice_steps.js
+++ features/step_definitions/one_practice_steps.js
@@ -10,7 +10,7 @@ var Steps = function () {
                 'domain':'www.codefordream.com'
             })
         });
-        this.spooky.thenOpen("http://www.codefordream.com/courses/js_basic/sections/section_0/practices/normal/practice_hello_world");
+        this.spooky.thenOpen("http://www.codefordream.com/courses/js_basic/sections/section_1/practices/normal/practice_code_comments");
         callback();
     });
     When(/^input correct answer of the practice and submit it$/,function(callback){
@@ -18,11 +18,11 @@ var Steps = function () {
             this.evaluate(function(){
                 var editor_manager_id = "editor_manager";
                 editor_manager = new EditorManager(editor_manager_id);
-                var file = document.getElementById("challenge_0");
+                var file = document.getElementById("challenge_1");
                 var isReadOnly = false;
-                file.value = window.decodeURIComponent("console.log('hello%20world')%3B");
-                editor_manager.files["challenge_0.js"] = file.value;
-                editor_manager.editors["challenge_0.js"] = editor_manager.create_editor(file,isReadOnly);
+                file.value = window.decodeURIComponent("%2F%2F%E5%9C%A8%E7%AC%AC%E4%BA%8C%E8%A1%8C%E7%9A%84%E5%BC%95%E5%8F%B7%E4%B8%AD%E8%BE%93%E5%85%A5%E4%BD%A0%E7%9A%84%E6%98%B5%E7%A7%B0%EF%BC%8C%E7%84%B6%E5%90%8E%E7%82%B9%E5%87%BB%22%E4%BF%9D%E5%AD%98%E5%B9%B6%E6%8F%90%E4%BA%A4%22%E6%8C%89%E9%92%AE%E4%BB%A5%E6%9F%A5%E7%9C%8B%E8%BF%90%E8%A1%8C%E7%BB%93%E6%9E%9C%E3%80%82%0Avar%20name%20%3D%20%22voctor%22%3B%0Aconsole.log('Hello%20'%20%2B%20name%20%2B%20'!')%3B%0A%2F*%0A%E7%AC%AC%E4%B8%89%E8%A1%8C%E4%BB%A3%E7%A0%81%E8%A2%AB%E8%BF%90%E8%A1%8C%E6%97%B6%EF%BC%8C%E5%8F%98%E9%87%8Fname%E4%BC%9A%E8%A2%AB%E6%9B%BF%E6%8D%A2%E6%88%90%E5%85%B6%E8%A2%AB%E8%B5%8B%E4%BA%88%E7%9A%84%E5%80%BC(%E8%BF%99%E9%87%8C%E6%98%AF%E4%BD%A0%E8%BE%93%E5%85%A5%E7%9A%84%E6%98%B5%E7%A7%B0%E5%AD%97%E7%AC%A6%E4%B8%B2)%E3%80%82%0A%22%2B%22%E5%8F%B7%E7%9A%84%E4%BD%9C%E7%94%A8%E6%98%AF%E5%B0%86%E5%A4%9A%E4%B8%AA%E5%AD%97%E7%AC%A6%E4%B8%B2%E6%8B%BC%E6%8E%A5%E6%88%90%E4%B8%80%E4%B8%AA%E5%AD%97%E7%AC%A6%E4%B8%B2%E3%80%82%0A*%2F");
+                editor_manager.files["challenge_1.js"] = file.value;
+                editor_manager.editors["challenge_1.js"] = editor_manager.create_editor(file,isReadOnly);
                 return 1;
             });
             this.emit('casper_log',1);
--- practice_config.js
+++ practice_config.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var readline = require("readline");
+var child_process = require('child_process');
 var practice_test_code = fs.readFileSync('./practice_test_template.js','utf-8');
 var practice_config_json = JSON.parse(fs.readFileSync('./practice_config.json','utf-8'));
 
@@ -12,12 +14,12 @@ function create_practice_test_file_content(one_practice_config_data){
     return practice_test_code;
 }
 
-function create_practice_test_file(){
+function create_practice_test_file(test_practice_name){
     fs.open('./features/step_definitions/one_practice_steps.js','w',0666,function(err,fd){
         if(err){
             console.log(err);
         }else{
-            fs.write(fd, create_practice_test_file_content(practice_config_json.hello_world), 0, 'utf8', function(err) {
+            fs.write(fd, create_practice_test_file_content(practice_config_json[test_practice_name]), 0, 'utf8', function(err) {
                 if(err) {
                     console.log('出错信息：' + err);
                 } else {
@@ -28,4 +30,29 @@ function create_practice_test_file(){
     })
 }
 
-create_practice_test_file();
+function get_testing_practice_name(){
+    var all_practices = '\n';
+    var index = 1;
+    for(var key in practice_config_json){
+        all_practices += index+'.'+key+'|';
+        index++;
+    }
+    console.log('All Test Practices:'+all_practices);
+    var reader = readline.createInterface(process.stdin, process.stdout, null);
+    reader.question("Which practice do you test,please input its name:", function(answer) {
+        if (answer in practice_config_json) {
+            create_practice_test_file(answer);
+            child_process.exec('cucumber.js ./features/one_practice.feature',function(){
+                console.log('Practice Test Succeed');
+            })
+            console.log('Testing...');
+            reader.close();
+        } else {
+            console.log('Sorry,no the practice')
+            reader.close();
+        }
+    });
+}
+
+get_testing_practice_name();
+
