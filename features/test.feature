9d6954640afa47603e42b95717f15d908a777771
weiketa <lyvoctor@gmail.com>
2014-11-10 09:15:39 +0800

    delete fils related feature config

--- features/test.feature
+++
@@ -1,6 +0,0 @@
-Feature:login on codefordream
-	Scenario:open code for dream
-		Given voctor on codefordream
-		When Code for dream voctor login
-		Then Welcome,voctor
-		And run
--- spooky_api.js
+++
@@ -1,7 +0,0 @@
-function find_spooky_api(api_key,api_param){
-    var spooky_api = {'open_url':'\nthis.spooky.thenOpen("@param")','then':'this.spooky.then(function(){@param})'}
-    var api_funciton = spooky_api[api_key].replace(/@param/,api_param);
-    return api_funciton;
-}
-
-module.exports = {find:find_spooky_api};
